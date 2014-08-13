1f28e68bd99ae505ea0ae55bc1f6a3419f28d0f8
Haakon Sporsheim <haakon@pexip.com>
2014-08-13 13:47:50 +0200

    rtpsession: Send and receive Microsoft x-pli
    This patch checks RTP send and receive caps for
    the field 'rtcp-fb-x-message-x-pli' in the same manner as other
    rtcp feedback key unit methods are checked for.
    
    The only difference from a standard PLI is the extra FCI blob which
    includes a 16bit request id and a 64 bit so called SFR. This patch
    ignores retransmissions of equal request ids when x-pli is turned on.
    
    The patch includes tests for fir, pli and x-pli, as well as
    a fix for making clear-pt-map actually work for gstrtpsession.

--- gst/rtpmanager/rtpsource.h
+++ gst/rtpmanager/rtpsource.h
@@ -36,6 +36,24 @@
 
 #define RTP_SEQ_MOD          (1 << 16)
 
+/**
+ * RTPKeyUnitType:
+ * @RTP_KEY_UNIT_NONE: No key unit method set.
+ * @RTP_KEY_UNIT_FIR:  Full Intra Request mehtod.
+ * @RTP_KEY_UNIT_PLI:  Picture Loss Indication mehtod.
+ * @RTP_KEY_UNIT_XPLI: Microsoft x-pli method.
+ *
+ * Key unit request types.
+ */
+typedef enum
+{
+  RTP_KEY_UNIT_NONE = -1,
+  RTP_KEY_UNIT_FIR = 0,
+  RTP_KEY_UNIT_PLI,
+  RTP_KEY_UNIT_XPLI,
+  RTP_KEY_UNIT_LAST
+} RTPKeyUnitType;
+
 typedef struct _RTPSource RTPSource;
 typedef struct _RTPSourceClass RTPSourceClass;
 
@@ -190,11 +208,17 @@ struct _RTPSource {
 
   GQueue        *retained_feedback;
 
-  gboolean     send_pli;
-  gboolean     send_fir;
-  guint8       current_send_fir_seqnum;
-  gint         last_fir_count;
-  GstClockTime last_keyframe_request;
+  RTPKeyUnitType key_unit_type;
+  guint8         current_send_fir_seqnum;
+  gint           last_fir_count;
+  GstClockTime   last_keyframe_request;
+
+  guint16        current_send_xpli_reqid;
+  guint64        current_send_xpli_sfr;
+
+  gboolean       recv_xpli;
+  gint           last_recv_xpli_reqid;
+  guint64        last_recv_xpli_sfr;
 
   gboolean     send_nack;
   GArray      *nacks;
