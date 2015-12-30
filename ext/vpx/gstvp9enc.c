c4c4be2dbe0d696b49d0ee606b325c80157bbc2a
Stian Selnes <stian@pexip.com>
2015-12-30 19:46:07 +0100

    vp9enc: Add 'bitrate' property as an alias to 'target-bitrate'

--- ext/vpx/gstvp9enc.c
+++ ext/vpx/gstvp9enc.c
@@ -129,6 +129,7 @@ enum
 {
   PROP_0,
   PROP_RC_END_USAGE,
+  PROP_BITRATE,
   PROP_RC_TARGET_BITRATE,
   PROP_RC_MIN_QUANTIZER,
   PROP_RC_MAX_QUANTIZER,
@@ -415,6 +416,12 @@ gst_vp9_enc_class_init (GstVP9EncClass * klass)
       g_param_spec_enum ("end-usage", "Rate control mode",
           "Rate control mode",
           GST_VP9_ENC_END_USAGE_TYPE, DEFAULT_RC_END_USAGE,
+          (GParamFlags) (G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS)));
+
+  g_object_class_install_property (gobject_class, PROP_BITRATE,
+      g_param_spec_int ("bitrate", "Target bitrate",
+          "Alias for target-bitrate property",
+          0, G_MAXINT, DEFAULT_RC_TARGET_BITRATE,
           (GParamFlags) (G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS)));
 
   g_object_class_install_property (gobject_class, PROP_RC_TARGET_BITRATE,
@@ -808,6 +815,7 @@ gst_vp9_enc_set_property (GObject * object, guint prop_id,
       gst_vp9_enc->cfg.rc_end_usage = g_value_get_enum (value);
       global = TRUE;
       break;
+    case PROP_BITRATE:
     case PROP_RC_TARGET_BITRATE:
       gst_vp9_enc->cfg.rc_target_bitrate = g_value_get_int (value) / 1000;
       gst_vp9_enc->rc_target_bitrate_set = TRUE;
@@ -1189,6 +1197,7 @@ gst_vp9_enc_get_property (GObject * object, guint prop_id, GValue * value,
     case PROP_RC_END_USAGE:
       g_value_set_enum (value, gst_vp9_enc->cfg.rc_end_usage);
       break;
+    case PROP_BITRATE:
     case PROP_RC_TARGET_BITRATE:
       g_value_set_int (value, gst_vp9_enc->cfg.rc_target_bitrate * 1000);
       break;
