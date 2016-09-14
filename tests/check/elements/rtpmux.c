7bcb0dc7cceff8928d30c574c107f7d2739f6174
Mikhail Fludkov <misha@pexip.com>
2016-09-14 16:41:02 +0200

    rtpssrcdemux: introduce max-streams property
    The property is useful against atacks when the sender changes SSRC for
    every RTP packet. The property with the same name introduced in rtpbin
    was not enough, because we still can end up with thousands of pads
    allocated in rtpssrcdemux.

--- tests/check/elements/rtpmux.c
+++ tests/check/elements/rtpmux.c
@@ -614,6 +614,40 @@ GST_START_TEST (test_rtpmux_ssrc_downstream_dynamic)
   gst_harness_teardown (h1);
   gst_harness_teardown (h);
 }
+GST_END_TEST;
+
+static void
+new_ssrc_pad_found (GstElement * element, guint ssrc, GstPad * pad,
+    GSList ** src_h)
+{
+  GstHarness *h;
+  (void) ssrc;
+
+  h = gst_harness_new_with_element (element, NULL, NULL);
+  gst_harness_add_element_src_pad (h, pad);
+  *src_h = g_slist_prepend (*src_h, h);
+}
+
+GST_START_TEST (test_rtpssrcdemux_max_streams)
+{
+  GstHarness *h = gst_harness_new_with_padnames ("rtpssrcdemux", "sink", NULL);
+  GSList *src_h = NULL;
+
+  g_object_set (h->element, "max-streams", 64, NULL);
+  gst_harness_set_src_caps_str (h, "application/x-rtp");
+  g_signal_connect (h->element,
+      "new-ssrc-pad", (GCallback) new_ssrc_pad_found, &src_h);
+  gst_harness_play (h);
+
+  for (int i = 0; i < 128; ++i) {
+    fail_unless_equals_int (GST_FLOW_OK,
+        gst_harness_push (h, generate_test_buffer (0, i)));
+  }
+
+  fail_unless_equals_int (g_slist_length (src_h), 64);
+  g_slist_free_full (src_h, (GDestroyNotify) gst_harness_teardown);
+  gst_harness_teardown (h);
+}
 
 GST_END_TEST;
 
@@ -641,6 +675,10 @@ rtpmux_suite (void)
 
   tc_chain = tcase_create ("rtpdtmfmux_lock");
   tcase_add_test (tc_chain, test_rtpdtmfmux_lock);
+  suite_add_tcase (s, tc_chain);
+
+  tc_chain = tcase_create ("rtpssrcdemux_basic");
+  tcase_add_test (tc_chain, test_rtpssrcdemux_max_streams);
   suite_add_tcase (s, tc_chain);
 
   return s;
