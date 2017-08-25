1766418f87d8c04c6fce06b49540d836c493cd4e
Mikhail Fludkov <misha@pexip.com>
2017-08-25 11:59:00 +0200

    rtpsession: Fix on-feedback-rtcp race
    If there is an external source which is about to timeout and be removed
    from the source hashtable and we receive feedback RTCP packet with the
    media ssrc of the source, we unlock the session in
    rtp_session_process_feedback before emitting 'on-feedback-rtcp' signal
    allowing rtcp timer to kick in and grab the lock. It will get rid of
    the source and rtp_session_process_feedback will be left with RTPSource
    with ref count 0.
    
    The fix is to grab the ref to the RTPSource object in
    rtp_session_process_feedback.

--- gst/rtpmanager/rtpsession.c
+++ gst/rtpmanager/rtpsession.c
@@ -2810,6 +2810,9 @@ rtp_session_process_feedback (RTPSession * sess, GstRTCPPacket * packet,
   if (sess->scheduled_bye && src && RTP_SOURCE_IS_MARKED_BYE (src))
     return;
 
+  if (src)
+    g_object_ref (src);
+
   fci_data = gst_rtcp_packet_fb_get_fci (packet);
   fci_length = gst_rtcp_packet_fb_get_fci_length (packet) * sizeof (guint32);
 
@@ -2877,6 +2880,9 @@ rtp_session_process_feedback (RTPSession * sess, GstRTCPPacket * packet,
         break;
     }
   }
+
+  if (src)
+    g_object_unref (src);
 }
 
 /**
