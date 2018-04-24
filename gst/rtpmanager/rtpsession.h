552596510a86197d7af3d2ec1b4ca1bd9dc3cac4
Stian Selnes <stian@pexip.com>
2018-04-24 17:49:59 +0200

    SQUASH: rtpsession: Add property "stats-min-interval" and notify "stats"

--- gst/rtpmanager/rtpsession.h
+++ gst/rtpmanager/rtpsession.h
@@ -287,7 +287,7 @@ struct _RTPSession {
   RTPSessionStats stats;
   RTPSessionStats bye_stats;
   GstClockTime    last_stats_notify_time;
-  guint           stats_min_interval_ms;
+  guint           stats_notify_min_interval_ms;
 
   gboolean      favor_new;
   GstClockTime  rtcp_feedback_retention_window;
