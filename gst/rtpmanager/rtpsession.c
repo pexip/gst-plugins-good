4581e547f63af33603a6d45866db33fea59d58e3
John Bassett <john.bassett@pexip.com>
2018-04-30 10:54:19 +0200

    rtpsession: Fix race when sending PLI, FIR and NACK packets
    Calling rtp_session_send_rtcp before marking the source as requiring a
    pli/fir/nack meant the rtcp_thread could be scheduled and start running
    before the source was updated. This meant the request would not be sent
    early but instead was transmitted with the next regular RTCP packet.
    
    Add test for nack generation.

--- gst/rtpmanager/rtpsession.c
+++ gst/rtpmanager/rtpsession.c
@@ -4499,11 +4499,6 @@ rtp_session_request_key_unit (RTPSession * sess, guint32 ssrc,
 {
   RTPSource *src;
 
-  if (!rtp_session_send_rtcp (sess, 5 * GST_SECOND)) {
-    GST_DEBUG ("FIR/PLI not sent");
-    return FALSE;
-  }
-
   RTP_SESSION_LOCK (sess);
   src = find_source (sess, ssrc);
   if (src == NULL)
@@ -4520,6 +4515,10 @@ rtp_session_request_key_unit (RTPSession * sess, guint32 ssrc,
     src->send_pli = TRUE;
   }
   RTP_SESSION_UNLOCK (sess);
+
+  if (!rtp_session_send_rtcp (sess, 5 * GST_SECOND)) {
+    GST_DEBUG ("FIR/PLI not sent early, sending with next regular RTCP");
+  }
 
   return TRUE;
 
@@ -4548,11 +4547,6 @@ rtp_session_request_nack (RTPSession * sess, guint32 ssrc, guint16 seqnum,
 {
   RTPSource *source;
 
-  if (!rtp_session_send_rtcp (sess, max_delay)) {
-    GST_DEBUG ("NACK not sent");
-    return FALSE;
-  }
-
   RTP_SESSION_LOCK (sess);
   source = find_source (sess, ssrc);
   if (source == NULL)
@@ -4561,6 +4555,10 @@ rtp_session_request_nack (RTPSession * sess, guint32 ssrc, guint16 seqnum,
   GST_DEBUG ("request NACK for %08x, #%u", ssrc, seqnum);
   rtp_source_register_nack (source, seqnum);
   RTP_SESSION_UNLOCK (sess);
+
+  if (!rtp_session_send_rtcp (sess, max_delay)) {
+    GST_DEBUG ("NACK not sent early, sending with next regular RTCP");
+  }
 
   return TRUE;
 
