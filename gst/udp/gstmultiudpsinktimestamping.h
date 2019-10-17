f9bced9eaab7d43d75d96deab2377f6dfd4d286f
Knut Saastad <knut@bitflow.io>
2019-10-17 11:14:39 +0200

    Move timestamping gsmultiudpsink related things into separate files.

---
+++ gst/udp/gstmultiudpsinktimestamping.h
@@ -0,0 +1,9 @@
+#ifndef __GST_MULTIUDPSINK_TIMESTAMPING_H__
+#define __GST_MULTIUDPSINK_TIMESTAMPING_H__
+
+#include <gst/gst.h>
+#include <gst/base/gstbasesink.h>
+#include <gio/gio.h>
+#include <gio/gunixtimestampingmessage.h>
+
+#endif
