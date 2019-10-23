12dee4d27af53c662834ab72b90e97faa4ae9363
Knut Saastad <Knut@bitflow.io>
2019-10-23 11:49:57 +0200

    Set control_message for iovec send if available in buffer.

--- gst/udp/gstmultiudpsink.c
+++ gst/udp/gstmultiudpsink.c
@@ -777,6 +777,8 @@ gst_multiudpsink_render_buffers (GstMultiUDPSink * sink, GstBuffer ** buffers,
 
   /* populate first num_buffers messages with output vectors for the buffers */
   for (i = 0, mem = 0; i < num_buffers; ++i) {
+    GstNetControlMessageMeta *meta = gst_buffer_get_net_control_message_meta (buffers[i]);
+
     size += fill_vectors (&vecs[mem], &map_infos[mem], mem_nums[i], buffers[i]);
     msgs[i].vectors = &vecs[mem];
     msgs[i].num_vectors = mem_nums[i];
@@ -784,6 +786,10 @@ gst_multiudpsink_render_buffers (GstMultiUDPSink * sink, GstBuffer ** buffers,
     msgs[i].bytes_sent = 0;
     msgs[i].control_messages = NULL;
     msgs[i].address = clients[0]->addr;
+
+    msgs[i].control_messages = (meta) ? meta->message : NULL;
+    msgs[i].num_control_messages = (meta && meta->message) ? 1 : 0;
+
     mem += mem_nums[i];
   }
 
